d7b57d0bad46fbbeb0e72ffe22ce96fc346652a1
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-21 10:41:39 -0500

    010 Using The Custom Hook In More Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
-import useHttp from './hooks/use-http';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/useHttp";
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -21,7 +21,9 @@ function App() {
     };
 
     fetchTasks(
-      { url: 'https://react-http-6b4a6.firebaseio.com/tasks.json' },
+      {
+        url: "https://react-http-69ae2-default-rtdb.firebaseio.com/tasks.json",
+      },
       transformTasks
     );
   }, [fetchTasks]);
